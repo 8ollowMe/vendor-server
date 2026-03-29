fe92f5864858e9b30998a99f344a6f574729cfaf
jsh9057 <jsh9057@gmail.com>
2026-03-29 16:50:18 +0900

    feat: vendor repository 구현체 기능 정의

--- src/main/java/com/followme/vendor_server/vendor/infrastructure/repository/VendorRepositoryImpl.java
+++ src/main/java/com/followme/vendor_server/vendor/infrastructure/repository/VendorRepositoryImpl.java
@@ -5,10 +5,18 @@ import com.followme.vendor_server.vendor.domain.VendorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @RequiredArgsConstructor
 @Repository
 public class VendorRepositoryImpl implements VendorRepository {
   private final VendorJpaRepository vendorJpaRepository;
+
+  @Override
+  public Optional<Vendor> findById(UUID vendorId) {
+    return vendorJpaRepository.findById(vendorId);
+  }
 
   @Override
   public Vendor save(Vendor vendor) {
