1bc91a617399ab7a7cae3535f65672e86d550da8
jsh9057 <jsh9057@gmail.com>
2026-03-29 21:07:57 +0900

    feat: 업체 정보 초기 수정 서비스 구현

---
+++ src/main/java/com/followme/vendor_server/vendor/application/UpdateVendorService.java
@@ -0,0 +1,50 @@
+package com.followme.vendor_server.vendor.application;
+
+import com.followme.vendor_server.vendor.application.command.UpdateVendorCommand;
+import com.followme.vendor_server.vendor.domain.Vendor;
+import com.followme.vendor_server.vendor.domain.VendorRepository;
+import com.followme.vendor_server.vendor.domain.exception.VendorErrorCode;
+import com.followme.vendor_server.vendor.domain.exception.VendorException;
+import com.followme.vendor_server.vendor.domain.service.HubExistenceChecker;
+import com.followme.vendor_server.vendor.domain.service.PermissionChecker;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class UpdateVendorService {
+  private final VendorRepository vendorRepository;
+  private final PermissionChecker permissionChecker;
+  private final HubExistenceChecker hubExistenceChecker;
+
+  /** TODO: result 값 수정 */
+  public Vendor updateVendorInfo(UpdateVendorCommand command) {
+    Vendor vendor =
+        vendorRepository
+            .findById(command.getVendorId())
+            .orElseThrow(() -> new VendorException(VendorErrorCode.VENDOR_NOT_FOUND));
+
+    vendor.updateInfo(
+        command.getHubId(),
+        command.getRequestId(),
+        command.getName(),
+        command.getVendorType(),
+        command.getDescription(),
+        command.getOwnerId(),
+        command.getOwnerName(),
+        command.getAddress(),
+        command.getLatitude(),
+        command.getLongitude(),
+        permissionChecker,
+        hubExistenceChecker);
+
+    /** TODO: updatedEvent 발행, outbox 를 하나의 트랜잭션으로 관리하는 기능 추가 해야 됨 */
+    log.info("Updated vendor with id {}", vendor.getId());
+
+    return vendor;
+  }
+}
