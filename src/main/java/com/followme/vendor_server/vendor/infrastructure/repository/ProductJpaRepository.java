fba2f59ffdfa993136e5add5e21c53e0d0b5a9ad
jsh9057 <jsh9057@gmail.com>
2026-03-30 10:13:19 +0900

    refactor: 사용하지않는 레포지토리 메서드 삭제

--- src/main/java/com/followme/vendor_server/vendor/infrastructure/repository/ProductJpaRepository.java
+++ src/main/java/com/followme/vendor_server/vendor/infrastructure/repository/ProductJpaRepository.java
@@ -7,6 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProductJpaRepository extends JpaRepository<Product, UUID> {
 
   boolean existsByVendor_Id_IdAndCode(UUID vendorId, String code);
-
-  boolean existsByVendor_Id_IdAndCodeAndId_IdNot(UUID vendorId, String code, UUID excludeProductId);
 }
