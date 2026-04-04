c921278aba00da96b6fbb992a1618bd7c659b004
jsh9057 <jsh9057@gmail.com>
2026-04-04 20:33:22 +0900

    feat: 공통 라이브러리 설정 반영
    - kfaka 사용시 공통 모듈 기능을 사용하기 위해 설정 추가

--- src/main/java/com/followme/vendor_server/VendorServerApplication.java
+++ src/main/java/com/followme/vendor_server/VendorServerApplication.java
@@ -2,8 +2,14 @@ package com.followMe.vendor_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@EnableScheduling
+@SpringBootApplication(scanBasePackages = "com.followMe")
+@EnableJpaRepositories(basePackages = "com.followMe")
+@EntityScan(basePackages = "com.followMe")
 public class VendorServerApplication {
 
   public static void main(String[] args) {
