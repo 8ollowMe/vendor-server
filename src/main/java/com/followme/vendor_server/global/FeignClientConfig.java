8b5e8030bade240e8b3528dbb9d53938e0574a06
jsh9057 <jsh9057@gmail.com>
2026-04-04 21:31:40 +0900

    fix: spotless 적용

--- src/main/java/com/followme/vendor_server/global/FeignClientConfig.java
+++ src/main/java/com/followme/vendor_server/global/FeignClientConfig.java
@@ -1,7 +1,7 @@
 package com.followMe.vendor_server.global;
 
 //
-///** TODO: 헤더정보 추가, 전파 */
-//@EnableFeignClients("com.followMe.vendor_server")
-//@Configuration
-//public class FeignClientConfig {}
+/// ** TODO: 헤더정보 추가, 전파 */
+// @EnableFeignClients("com.followMe.vendor_server")
+// @Configuration
+// public class FeignClientConfig {}
--- src/main/java/com/followme/vendor_server/global/JpaAuditConfig.java
+++ src/main/java/com/followme/vendor_server/global/JpaAuditConfig.java
@@ -1,6 +1,5 @@
 package com.followMe.vendor_server.global;
 
-
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.context.annotation.Bean;
